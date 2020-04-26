9cae87efd5e7d12ba4dc87dfe96f89c248e60715
Carl Schmidt <PeopleGitReady>
2020-04-26 14:48:27 +0200

    Built the basic layout for the home page. Buttons are not doing anything yet when clicked.

--- flutter/lib/pages/home_page.dart
+++ flutter/lib/pages/home_page.dart
@@ -13,17 +13,91 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text("Home Page Lol"),
+      title: Text("Parking App", style: TextStyle(color: Colors.white)),
+      backgroundColor: Colors.greenAccent,
       actions: <Widget>[
         IconButton(
-          icon: Icon(Icons.settings_applications),
+          icon: Icon(Icons.settings, color: Colors.white, size: 40),
           tooltip: "Settings",
           onPressed: () {
             openPage(context);
           },
         )
       ],
-    ));
+    ),
+        body: Container(
+            padding: EdgeInsets.only(left: 45, right: 45),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 30),
+                RaisedButton(
+                    padding: const EdgeInsets.all(16),
+                    onPressed: () => {},
+                    color: Colors.greenAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(5)
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.favorite, size: 45, color: Colors.white),
+                        SizedBox(width: 5),
+                        Text(
+                            "Favourites",
+                            style: TextStyle(
+                              fontSize: 20,
+                            )
+                        )
+                      ],
+                    )
+                ),
+                SizedBox(height: 20),
+                RaisedButton(
+                    padding: const EdgeInsets.all(16),
+                    onPressed: () => {},
+                    color: Colors.greenAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(7)
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.person_pin_circle, size: 45, color: Colors.white),
+                        SizedBox(width: 5),
+                        Text(
+                            "Close to me",
+                            style: TextStyle(
+                              fontSize: 20,
+                            )
+                        )
+                      ],
+                    )
+                ),
+                SizedBox(height: 20),
+                RaisedButton(
+                    padding: const EdgeInsets.all(16),
+                    onPressed: () => {},
+                    color: Colors.greenAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(7)
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.search, size: 45, color: Colors.white),
+                        SizedBox(width: 5),
+                        Text(
+                            "Search",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                        ),
+                      ],
+                    ),
+                )
+              ],
+            ),
+        ),
+    );
   }
 }
 
