b50486e4da8b994f266089a8e467fba9f4ec3756
Enzo <enzito@protonmail.com>
2020-05-11 13:12:47 +0200

    Pinning on the map done. URI code generator on the way.

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ezsgame/pages/map_page.dart';
 import 'package:ezsgame/pages/root_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -9,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.white,
-        ),
-        home: new RootPage(auth: new Auth())
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      //home: new RootPage(auth: new Auth())
+      home: new MapPage(),
     );
   }
 }
