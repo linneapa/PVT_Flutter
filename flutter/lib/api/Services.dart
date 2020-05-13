ffc001733e726ef1145afaf8c5e0db1d7680838a
d501 <ekb@protonmail.com>
2020-05-13 17:52:33 +0200

    filters is back baby

--- flutter/lib/api/Services.dart
+++ flutter/lib/api/Services.dart
@@ -4,18 +4,27 @@ import 'package:http/http.dart' as http;
 import 'ParkingSpace.dart';
 
 class Services {
-  static Future<Parkering> fetchParkering(bool _globalCarToggled, bool _globalTruckToggled, bool _globalMotorcycleToggled, bool handicapToggled) async {
-    // :TODO Here must come the filter logic (avstånd, vehicle type, address n shit)
-    final response = await http.get(
-      // https://openparking.stockholm.se/LTF-Tolken/v1/{föreskrift}/{operation}?apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97
+  static Future<Parkering> fetchParkering(bool car, bool lastbil, bool motorcyckel, bool handicaped) async {
+    // https://openparking.stockholm.se/LTF-Tolken/v1/{föreskrift}/{operation}?apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97
 
-      /*
+    /*
       Föreskrift: servicedagar, ptillaten, pbuss, plastbil, pmotorcykel, prorelsegindrad
       Operation: all, weekday, area, street, within, untilNextWeekday
       Parameters: apiKey, MaxFeatures, outputFormat, callback
        */
-        'https://openparking.stockholm.se/LTF-Tolken/v1/servicedagar/weekday/m%C3%A5ndag?outputFormat=json&apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97');
-
+    String url;
+    if (car) {
+      url = 'https://openparking.stockholm.se/LTF-Tolken/v1/ptillaten/all?maxFeatures=100&outputFormat=json&apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97';
+    } else if(lastbil) {
+      url = 'https://openparking.stockholm.se/LTF-Tolken/v1/plastbil/all?outputFormat=json&apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97';
+    } else if (motorcyckel){
+      url = 'https://openparking.stockholm.se/LTF-Tolken/v1/pmotorcykel/all?maxFeatures=100&outputFormat=json&apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97';
+    } else if (handicaped){
+      url = 'https://openparking.stockholm.se/LTF-Tolken/v1/prorelsehindrad/all?&maxFeatures=100&outputFormat=json&apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97';
+    } else {
+      return null;
+    }
+    final response = await http.get(url);
     if (response.statusCode == 200) {
       return Parkering.fromJson(json.decode(response.body));
     } else {
