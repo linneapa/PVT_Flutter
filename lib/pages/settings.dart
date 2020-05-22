36eb45ba9775f78e0f9ae3ccf363eab06d93dfec
Julius <julius.bostrom@gmail.com>
2020-05-22 13:31:32 +0200

    Started working on settings. No functionality yet.

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ezsgame/firebase/authentication.dart';
+import 'package:ezsgame/pages/SizeConfig.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key key, this.auth, this.logoutCallback})
@@ -14,6 +15,7 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
 
+  SizeConfig sizeConfig;
 
   signOut() async {
     try {
@@ -27,11 +29,13 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
+    sizeConfig = SizeConfig();
+    sizeConfig.init(context);
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text("Inställningar", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.orangeAccent,
         actions: <Widget>[
           new FlatButton(
               child: new Text(
@@ -40,8 +44,50 @@ class _SettingsPageState extends State<SettingsPage> {
         ],
       ),
       body: Container(
-          child: Column(
-          )
+        child:ListView(
+          children: ListTile.divideTiles(
+              context: context,
+              tiles: [ //TEMPORARY ITEMS
+                ListTile(
+                  contentPadding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 2.2, bottom: SizeConfig.blockSizeVertical * 2.2, left: SizeConfig.blockSizeHorizontal * 4, right: SizeConfig.blockSizeHorizontal * 4),
+                  title: Text('Kontoinställningar',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: SizeConfig.blockSizeVertical * 3.5
+                    ),
+                  ),
+                  subtitle: Text('Ändra lösenord, etc. (Denna text kommer ändras)',
+                    style: TextStyle(
+                      fontSize: SizeConfig.blockSizeVertical * 2.2
+                    )
+                  ),
+                  onTap: () {
+                    //Do something
+                  },
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 2.2, bottom: SizeConfig.blockSizeVertical * 2.2, left: SizeConfig.blockSizeHorizontal * 4, right: SizeConfig.blockSizeHorizontal * 4),
+                  title: Text('Standardavstånd',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: SizeConfig.blockSizeVertical * 3.5
+                    ),
+                  ),
+                  subtitle: Text('Ställ in radien från din destination du vill se parkeringar',
+                    style: TextStyle(
+                      fontSize: SizeConfig.blockSizeVertical * 2.2
+                    )
+                  ),
+                  onTap: () {
+                    //Do something
+                  },
+                ),
+                ListTile(
+                  //Empty ListTile to draw divider below the item above
+                ),
+              ],
+          ).toList(growable: false),
+        ),
       ),
     );
   }
